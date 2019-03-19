082b8a97b31d252378ec2f2562f867d4d49bc5c2
WouterVer <wouterverschueren@hotmail.com>
2019-03-19 13:12:55 +0100

    Update to exponential recovery test and atlernative model

--- test/nodedynamics.jl
+++ test/nodedynamics.jl
@@ -142,16 +142,17 @@ end
 
 @testset "ExponetialRec" begin
 @syms V0 Nps Npt Nqs Nqt Tp Tq positive=true
-@syms P0 Q0 real=true
+@syms P0 Q0 Pd Qd real=true
 @syms x_p dx_p x_q dx_q real=true
-VSIdyn = construct_node_dynamics(ExponentialRec(P0, Q0, Nps, Npt, Nqs, Nqt, Tp, Tq, V0))
+ExpRec = construct_node_dynamics(ExponentialRec(P0=P0, Q0=Q0, Nps=Nps, Npt=Npt, Nqs=Nqs, Nqt=Nqt, Tp=Tp, Tq=Tq, V0=V0))
 dint = [dx_p,dx_q]; int = [x_p,x_q]; int_test = copy(int)
-Pd = real(u*conj(i))
-Qd = imag(u*conj(i))
+ExpRec.ode_dynamics.rhs(dint, u, i, int, t)
 #dx_p = (1/Tp)*(-x_p + P0*((abs(u)/V0)^Nps) - P0*((abs(u)/V0)^Npt))
 #dx_q = (1/Tq)*(-x_q + Q0*((abs(u)/V0)^Nqs) - Q0*((abs(u)/V0)^Nqt))
 #du = (-Pd + x_p + P0*((abs(u)/V0)^Npt)) + im*(-Qd + x_q + Q0*((abs(u)/V0)^Nqt))
-@test du == expand((-Pd + x_p + P0*((abs(u)/V0)^Npt)) + im*(-Qd + x_q + Q0*((abs(u)/V0)^Nqt)))
-@test expand.(dint[1]) == expand.((1/Tp)*(-x_p + P0*((abs(u)/V0)^Nps) - P0*((abs(u)/V0)^Npt)))
-@test expand.(dint[2]) == expand.((1/Tq)*(-x_q + Q0*((abs(u)/V0)^Nqs) - Q0*((abs(u)/V0)^Nqt)))
+@test expand(dint[1]) == expand((1/Tp)*(-x_p + P0*(v/V0)^Nps - P0*(v/V0)^Npt ))
+@test expand(dint[2]) == expand((1/Tq)*(-x_q + Q0*(v/V0)^Nqs - Q0*(v/V0)^Nqt ))
+
+
+@test ExpRec.ode_dynamics.rhs(dint, u, i, int, t) == expand.((-p + x_p + P0*((v/V0)^Npt)) + im*(-q + x_q + Q0*((v/V0)^Nqt)))
 end
